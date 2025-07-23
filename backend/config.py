274aa2fefe11eed0d5156085a9111af101be833c
Jatinkumar K Parmar <jatinparmar1053@gmail.com>
2025-07-23 11:14:21 +0200

    Add FastAPI backend with config and server setup

---
+++ backend/config.py
@@ -0,0 +1,9 @@
+API_URL = "http://img_fastapi:8001"
+
+DB_CONFIG = {
+    "dbname": "img_db",
+    "user": "img_user",
+    "password": "img_pass",
+    "host": "img_postgres",
+    "port": "5432"
+}
